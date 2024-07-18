4f8474bd81c19676f5acab2010496e52970e42ec
Salman <salmanfaris98@gmail.com>
2024-07-18 10:12:01 +0100

    add: use absl in pca

--- src/pca.py
+++ src/pca.py
@@ -6,27 +6,29 @@ from sklearn.preprocessing import StandardScaler
 
 from pathlib import Path
 
+from absl import app, logging
+
 plt.style.use("bmh")
 
 SRC_DIR = Path(__file__).parents[1]
 DATASET_DIR = SRC_DIR / "datasets"
-FIGURE_DIR = SRC_DIR / "output" / "figures"
-FIGURE_DIR.mkdir(parents=True, exist_ok=True)
+PCA_FIGURE_DIR = SRC_DIR / "output" / "figures" / "pca"
+PCA_FIGURE_DIR.mkdir(parents=True, exist_ok=True)
 
 
-def main():
+def main(_):
     data = pd.read_csv(DATASET_DIR / "02_staged" / "processed_paysim.csv")
 
     # Filter for transfer types where there is fraudulent transactions (i.e.
     # `is_fraud` == 1). Only consider these types to perform PCA.
     unique_transfer_types = data.groupby("is_fraud")["type"].value_counts().loc[1].index
-    print(
-        f"Detected {len(unique_transfer_types)} transfer types with fraudulent"
+    logging.info(
+        f"Detected {len(unique_transfer_types)} transfer types with fraudulent "
         f"transaction. These are: [{', '.join(unique_transfer_types)}]."
     )
 
     for transfer_type in unique_transfer_types:
-        print(f"Performing PCA for type: {transfer_type}")
+        logging.info(f"Performing PCA for type: {transfer_type}")
         transfer_type_data = data.query(f"type == '{transfer_type}'")
 
         X = transfer_type_data.iloc[:, :-2]
@@ -51,10 +53,10 @@ def main():
             )
             ax.set_title(f"PCA for {transfer_type} transactions")
             fig.tight_layout()
-            fig.savefig(FIGURE_DIR / f"pca_{transfer_type}.png")
+            fig.savefig(PCA_FIGURE_DIR / f"pca_{transfer_type}.png")
 
         visualize_and_save_pca()
 
 
 if __name__ == "__main__":
-    main()
+    app.run(main)
