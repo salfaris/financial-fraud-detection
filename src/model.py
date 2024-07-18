4e34e5c850dce4a9c94dd5eb8932b13cdf98f16c
Salman <salmanfaris98@gmail.com>
2024-07-18 10:13:13 +0100

    update: metrics and add new model
    - New FPR metric to be used as stopping metric.
    - Add SVC with RBF kernel
    - Change result directory path

--- src/model.py
+++ src/model.py
@@ -4,10 +4,12 @@ import numpy as np
 import pandas as pd
 
 from sklearn.linear_model import LogisticRegression
-from sklearn.svm import LinearSVC
-from sklearn.metrics import precision_recall_fscore_support
+from sklearn.svm import LinearSVC, SVC
+from sklearn.metrics import precision_recall_fscore_support, recall_score
 
 from absl import app, flags, logging
+
+from config import FEATURE_NAMES, TARGET_NAME
 
 flags.DEFINE_enum(
     "model_name",
@@ -26,17 +28,8 @@ RNG = np.random.RandomState(10062930)
 
 ROOT_DIR = Path(__file__).parents[1]
 DATASET_DIR = ROOT_DIR / "datasets"
-RESULT_DIR = ROOT_DIR / "output" / "report"
+RESULT_DIR = ROOT_DIR / "output" / "result"
 RESULT_DIR.mkdir(exist_ok=True, parents=True)
-
-FEATURE_NAMES = [
-    "amount",
-    "old_balance_Source",
-    "new_balance_Source",
-    "old_balance_Destination",
-    "new_balance_Destination",
-]
-TARGET_NAME = "is_fraud"
 
 MODEL_FUNCTIONS = {
     "logreg": lambda cw: LogisticRegression(class_weight=cw, random_state=RNG),
@@ -45,6 +38,13 @@ MODEL_FUNCTIONS = {
         tol=1e-5,
         max_iter=1000,
         dual=False,
+        random_state=RNG,
+    ),
+    "svc_rbf": lambda cw: SVC(
+        class_weight=cw,
+        kernel="rbf",  # Default kernel but want to emphasize.
+        tol=1e-3,
+        cache_size=1000,
         random_state=RNG,
     ),
 }
@@ -77,6 +77,7 @@ def train(model_name: str, model_fn: callable):
         "precision": [],
         "recall": [],
         "f1-score": [],
+        "false_positive_rate": [],
     }
     max_class_weight = 512
     fraud_class_weights = range(1, max_class_weight + 1)
@@ -99,15 +100,20 @@ def train(model_name: str, model_fn: callable):
         metrics["recall"].append(recall)
         metrics["f1-score"].append(f1_score)
 
+        # FalsePositiveRate = 1 - TrueNegativeRate = 1 - Recall(with 0 as target label)
+        false_positive_rate = 1 - recall_score(y_true=y_val, y_pred=y_pred, pos_label=0)
+        metrics["false_positive_rate"].append(false_positive_rate)
+
         logging.info(
             f"class_weight: {fraud_weight}, precision: {precision:.5f}, "
-            f"recall: {recall:.5f}, F1-score: {f1_score:.5f}"
+            f"recall: {recall:.5f}, F1-score: {f1_score:.5f}, "
+            f"FPR: {false_positive_rate:.5f}"
         )
 
     metrics_df = pd.DataFrame(metrics)
     metrics_df.insert(0, "class_weight", list(fraud_class_weights))
     metrics_df.to_csv(
-        RESULT_DIR / f"result_{transaction_type.upper()}_{model_name}.csv",
+        RESULT_DIR / f"result_{FLAG.transaction_type.upper()}_{model_name}.csv",
         index=False,
     )
 
