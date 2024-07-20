ae2cf94ac9c3f8b7cb00d01e2040b9b2f93dc85d
Salman <salmanfaris98@gmail.com>
2024-07-20 12:32:59 +0100

    update: shell script with new module names

--- 02_run_model.sh
+++ 02_run_model.sh
@@ -3,13 +3,13 @@ eval "$(conda shell.bash hook)"
 conda activate dev
 
 echo "Running Logistic Regression..."
-python src/model.py --model_name logreg --transaction_type TRANSFER
-python src/model.py --model_name logreg --transaction_type CASH_OUT
+python src/evaluate_model_metrics.py --model_name logreg --transaction_type TRANSFER
+python src/evaluate_model_metrics.py --model_name logreg --transaction_type CASH_OUT
 
 echo "Running Linear SVC..."
-python src/model.py --model_name svc_linear --transaction_type TRANSFER
-python src/model.py --model_name svc_linear --transaction_type CASH_OUT
+python src/evaluate_model_metrics.py --model_name svc_linear --transaction_type TRANSFER
+python src/evaluate_model_metrics.py --model_name svc_linear --transaction_type CASH_OUT
 
 echo "Running SVC with RBF kernel..."
-python src/model.py --model_name svc_rbf --transaction_type TRANSFER
-python src/model.py --model_name svc_rbf --transaction_type CASH_OUT
+python src/evaluate_model_metrics.py --model_name svc_rbf --transaction_type TRANSFER
+python src/evaluate_model_metrics.py --model_name svc_rbf --transaction_type CASH_OUT
--- 03_run_find_ideal_class_weight.sh
+++ 03_run_find_ideal_class_weight.sh
@@ -3,13 +3,13 @@ eval "$(conda shell.bash hook)"
 conda activate dev
 
 echo "Running Logistic Regression..."
-python src/find_ideal_class_weight.py --model_name logreg --transaction_type TRANSFER
-python src/find_ideal_class_weight.py --model_name logreg --transaction_type CASH_OUT
+python src/evaluate_ideal_class_weight.py --model_name logreg --transaction_type TRANSFER
+python src/evaluate_ideal_class_weight.py --model_name logreg --transaction_type CASH_OUT
 
 echo "Running Linear SVC..."
-python src/find_ideal_class_weight.py --model_name svc_linear --transaction_type TRANSFER
-python src/find_ideal_class_weight.py --model_name svc_linear --transaction_type CASH_OUT
+python src/evaluate_ideal_class_weight.py --model_name svc_linear --transaction_type TRANSFER
+python src/evaluate_ideal_class_weight.py --model_name svc_linear --transaction_type CASH_OUT
 
 echo "Running SVC with RBF kernel..."
-python src/find_ideal_class_weight.py --model_name svc_rbf --transaction_type TRANSFER
-python src/find_ideal_class_weight.py --model_name svc_rbf --transaction_type CASH_OUT
+python src/evaluate_ideal_class_weight.py --model_name svc_rbf --transaction_type TRANSFER
+python src/evaluate_ideal_class_weight.py --model_name svc_rbf --transaction_type CASH_OUT
