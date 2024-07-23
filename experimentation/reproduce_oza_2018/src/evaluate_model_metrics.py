866d3b6ac96472c12b48f7c219e09d4fdd2aad89
Salman <salmanfaris98@gmail.com>
2024-07-23 21:01:46 +0100

    add: save and load model when evaluating metric

--- experimentation/reproduce_oza_2018/src/evaluate_model_metrics.py
+++ experimentation/reproduce_oza_2018/src/evaluate_model_metrics.py
@@ -1,4 +1,4 @@
-from joblib import Parallel, delayed
+from joblib import Parallel, delayed, dump, load
 from pathlib import Path
 
 import numpy as np
@@ -28,10 +28,12 @@ RNG = np.random.RandomState(10062930)
 ROOT_DIR = Path(__file__).parents[1]
 DATASET_DIR = ROOT_DIR / "datasets"
 RESULT_DIR = ROOT_DIR / "output" / "result"
+MODEL_DIR = ROOT_DIR / "output" / "model"
 RESULT_DIR.mkdir(exist_ok=True, parents=True)
 
 
 def train(model_name: str, model_fn: callable):
+    logging.info("Reading experiment datasets...")
     transaction_types = {
         "CASH_OUT": {},
         "TRANSFER": {},
@@ -60,29 +62,73 @@ def train(model_name: str, model_fn: callable):
         "f1-score": [],
         "false_positive_rate": [],
     }
-    max_class_weight = 512
+    max_class_weight = 10
     fraud_class_weights = range(1, max_class_weight + 1)
 
+    def load_model(model_path: Path):
+        return load(model_path)
+
+    def save_model(model, model_path: Path):
+        with open(model_path, "wb") as f:
+            dump(model, f)
+
     # Model training
-    def train_model(X, y, class_weight):
+    def train_model(X, y, class_weight, model_path):
         model = model_fn(class_weight)
         model.fit(X, y)
         print(
-            f"RUN: Training model `{model_name}` for "
+            f"DONE: Training model `{model_name}` for "
             f"class weight = {class_weight[1]}"
         )
+        save_model(model, model_path)
         return model, class_weight
 
+    model_subdir = MODEL_DIR / model_name
+    model_subdir.mkdir(exist_ok=True, parents=True)
+    logging.info(
+        "RUN: Adding training step to queue for all "
+        f"class weights 1...{max_class_weight}"
+    )
+    fraud_class_weights_to_train: list[int] = []
     delayed_train_models = []
     for fraud_weight in fraud_class_weights:
+        model_path = (
+            model_subdir
+            / f"{model_name}_{FLAG.transaction_type}_CW{str(fraud_weight).zfill(3)}.pkl"
+        )
+        if model_path.exists():
+            logging.info(
+                f"SKIP: Model `{model_name}` already exists @ {model_path.relative_to(ROOT_DIR)}."
+            )
+            continue
+        else:
+            logging.info(f"RUN: Adding training step for class weight = {fraud_weight}")
+            fraud_class_weights_to_train.append(fraud_weight)
         class_weight = {0: 1, 1: fraud_weight}
-        delayed_train_model = delayed(train_model)(X_train, y_train, class_weight)
+        delayed_train_model = delayed(train_model)(
+            X_train, y_train, class_weight, model_path
+        )
         delayed_train_models.append(delayed_train_model)
-    trained_models = Parallel(n_jobs=4)(delayed_train_models)
+    logging.info(
+        f"BEGIN: Training {len(fraud_class_weights_to_train)} models in parallel..."
+    )
+    trained_models = Parallel(n_jobs=6)(delayed_train_models)
 
-    logging.info("DONE: Training models in parallel. Ready to compute metrics.")
+    logging.info("DONE: Done training models. Ready to compute metrics.")
 
-    for model, class_weight in trained_models:
+    ready_models = []
+    fraud_weights = list(map(lambda x: x[1][1], trained_models))
+    for weight in range(1, max_class_weight + 1):
+        if weight not in fraud_weights:
+            model = load_model(
+                model_subdir
+                / f"{model_name}_{FLAG.transaction_type}_CW{str(weight).zfill(3)}.pkl"
+            )
+            ready_models.append((model, {0: 1, 1: weight}))
+    ready_models.extend(trained_models)
+    ready_models = sorted(ready_models, key=lambda x: x[1][1])
+
+    for model, class_weight in ready_models:
         fraud_weight = class_weight[1]
 
         y_pred = model.predict(X_val)
