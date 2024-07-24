6b958a564f1809146755d1d8d046dacba93e000a
Salman <salmanfaris98@gmail.com>
2024-07-24 16:21:33 +0100

    add: svc + rbf kernel approximation to evaluation between models

--- experimentation/reproduce_oza_2018/src/evaluate_between_ideal_model_performance.py
+++ experimentation/reproduce_oza_2018/src/evaluate_between_ideal_model_performance.py
@@ -8,6 +8,7 @@ from sklearn.metrics import PrecisionRecallDisplay
 from absl import app, flags, logging
 
 from model_config import FEATURE_NAMES, TARGET_NAME, MODEL_FUNCTIONS
+import utils
 
 mpl.rcParams["font.family"] = "Arial"
 
@@ -20,12 +21,14 @@ FLAG = flags.FLAGS
 
 ROOT_DIR = Path(__file__).parents[1]
 DATASET_DIR = ROOT_DIR / "datasets"
+MODEL_DIR = ROOT_DIR / "output" / "model"
 RESULT_DIR = ROOT_DIR / "output" / "result"
 RESULT_FIG_DIR = ROOT_DIR / "output" / "figures" / "result_model_comparison"
 RESULT_FIG_DIR.mkdir(exist_ok=True, parents=True)
 
 
 def main(_):
+    logging.info("BEGIN: Reading ideal class weight data...")
     icw_data = pd.read_csv(RESULT_DIR / "result_ideal_class_weight.csv")
 
     transaction_types = {
@@ -33,6 +36,9 @@ def main(_):
         "TRANSFER": {},
     }
     for transaction_type in transaction_types:
+        logging.info(
+            f"RUN: Loading {transaction_type} train, val, test data into memory..."
+        )
         transaction_types[transaction_type]["train"] = pd.read_csv(
             DATASET_DIR / "03_features" / f"{transaction_type}_train.csv", index_col=0
         )
@@ -57,43 +63,74 @@ def main(_):
         y_test.values.ravel(),
     )
 
+    def _get_model_with_type_dir(model_name: str, transaction_type: str) -> Path:
+        model_with_type_dir = MODEL_DIR / model_name / transaction_type
+        model_with_type_dir.mkdir(exist_ok=True, parents=True)
+        return model_with_type_dir
+
     fitted_models = {model_name: None for model_name in MODEL_FUNCTIONS}
     for model_name, model_fn in MODEL_FUNCTIONS.items():
+        logging.info(f"BUILD: Building model '{model_name}'...")
         model_icw_data = icw_data.query(
             f"model_name == '{model_name}' and "
             f"transaction_type == '{FLAG.transaction_type}'"
         )
         if model_icw_data.empty:
             logging.warning(
-                f"SKIPPING: No ideal class weight found for model_name='{model_name}'."
+                f"SKIP: No ideal class weight found for model_name='{model_name}'."
             )
             continue
         icw = model_icw_data.iloc[0].class_weight
-        model = model_fn(cw={0: 1, 1: icw})
-        model.fit(X_train, y_train)  # model was always trained on training set.
+
+        model_with_type_path = (
+            _get_model_with_type_dir(model_name, FLAG.transaction_type)
+            / f"{model_name}_{FLAG.transaction_type}_CW{str(icw).zfill(3)}.pkl"
+        )
+        if model_with_type_path.exists():
+            logging.info(
+                f"  BUILD: Model already exists. Loading model '{model_name}' from disk "
+                f"@ {model_with_type_path.relative_to(ROOT_DIR)}..."
+            )
+            model = utils.load_model(model_with_type_path)
+        else:
+            model = model_fn(cw={0: 1, 1: icw})
+            model.fit(X_train, y_train)  # model was always trained on training set.
+            utils.save_model(model, model_with_type_path)
         fitted_models[model_name] = model
 
-        # SAVE MODEL PARAMS HERE!
+    # def viz():
+    logging.info("RUN: Visualising PRC for model with ideal class weights...")
+    fig, ax = plt.subplots(figsize=(5, 5))
 
-    def viz():
-        fig, ax = plt.subplots(figsize=(5, 5))
+    for model_name, fitted_model in fitted_models.items():
+        if fitted_model is None:
+            logging.warning(
+                f"SKIP: skip visualising model_name='{model_name}' as no fitted "
+                "model found."
+            )
+            continue
+        if model_name == "svc_rbf" and FLAG.transaction_type == "CASH_OUT":
+            model_with_type_dir = _get_model_with_type_dir(
+                model_name, FLAG.transaction_type
+            )
+            scaler = utils.load_model(model_with_type_dir / "standard_scaler.pkl")
+            rbf_sampler = utils.load_model(model_with_type_dir / "rbf_sampler.pkl")
+            X_val_transformed = rbf_sampler.transform(scaler.transform(X_val))  # noqa
 
-        for model_name, fitted_model in fitted_models.items():
-            if fitted_model is None:
-                logging.warning(
-                    f"SKIPPING: No fitted model found for model_name='{model_name}'."
-                )
-                continue
+            PrecisionRecallDisplay.from_estimator(
+                fitted_model, X_val_transformed, y_val, pos_label=1, ax=ax
+            )
+        else:
             PrecisionRecallDisplay.from_estimator(
                 fitted_model, X_val, y_val, pos_label=1, ax=ax
             )
 
-        ax.set_title(f"Precision-Recall Curve for {FLAG.transaction_type} transactions")
+    ax.set_title(f"Precision-Recall Curve for {FLAG.transaction_type} transactions")
 
-        fig.tight_layout()
-        fig.savefig(RESULT_FIG_DIR / f"model_comparison_{FLAG.transaction_type}.png")
+    fig.tight_layout()
+    fig.savefig(RESULT_FIG_DIR / f"model_comparison_{FLAG.transaction_type}.png")
 
-    viz()
+    # viz()
 
 
 if __name__ == "__main__":
